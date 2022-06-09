d13f4d5d00ed59ee4c797f658ba7aef56580dc7b
Noah van Ekdom <noahvanekdom@gmail.com>
2022-06-09 12:17:06 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -5,23 +5,34 @@
 # The downcase method is called on the string object "Hello World"
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
-"Hello World".downcase
+p "Hello World".downcase
 
 # YOU DO the rest:
 
-
-"Hello World".include?("Hello")
-
-
-"Hello World".end_with?("Hello")
-
-
-"Hello World".end_with?("rld")
+# The include question mark method is called on the string object "Hello World".
+# Argument passed is the string "Hello", which the include question mark method
+# will check our object for any section that includes the string "Hello"
+# because "Hello" is contained within "Hello World", the return value is true
+p "Hello World".include?("Hello")
 
 
-12.even?
+# The ends_with question mark method is called on the string object "Hello World".
+# Argument passed is the string "Hello", the method will check our object "Hello World"
+# to see if it ends with the string "Hello". Because the object does not end with the String
+# "hello", it will return false.
+p "Hello World".end_with?("Hello")
 
+# The ends_with question mark method is called on the string object "Hello World".
+# Argument passed is the string "Hello", the method will check our object "Hello World"
+# to see if it ends with the string "rld". Because the object does end with the String
+# "rld", it will return true.
+p "Hello World".end_with?("rld")
 
-18.next
+# The even question mark method is called on the integer object 12, which will evaluate
+# the object to determine if it has a remainder when divided by two. If the remainder is 0,
+# it will return true. Since the remainder of 12 divided by 2 is zero, it will return true.
+p 12.even?
 
-
+# The next method is called on the integer 18. This method will return the next highest integer
+# following the object, which will produce a return of 19
+p 18.next
